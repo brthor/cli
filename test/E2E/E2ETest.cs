bc68aef091cf47673be036604a32085b14d63674
Bryan Thornbury <brthor@microsoft.com>
2015-12-18 01:29:07 +0000

    reenable e2e test on ubuntu

--- test/E2E/E2ETest.cs
+++ test/E2E/E2ETest.cs
@@ -59,11 +59,6 @@ namespace ConsoleApplication
                 return;
             }
 
-            if(SkipForOS(OSPlatform.Linux, "https://github.com/dotnet/cli/issues/527"))
-            {
-                return;
-            }
-
             TestSetup();
 
             TestRunCommand("dotnet", $"compile --native -o {OutputDirectory}");
